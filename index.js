f4f8a6ac30d9398c7443a1a434791f7caa0142ef
daniellasheva <daniellasheva@gmail.com>
2016-11-10 11:00:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById(".gradient-menu").addEventListener("click", function(){
+//     alert("hello!");
+// });
+
+
+// document.getElementById("").addEventListener("click", function(){
+//     alert("hello!");
+// });
+
+
+// document.getElementById("gradient-menu").addEventListener("click", function(){
+//   console.log("hi");
+// });
